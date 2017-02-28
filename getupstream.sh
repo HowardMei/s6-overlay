57b7cf9332be1ed3254a8600a42afaafe36136b0
Howard Mei <howardleomei@gmail.com>
2017-02-28 09:21:51 +0800

    dev_misc: add helper scripts to codify the upgrade flow

---
+++ getupstream.sh
@@ -0,0 +1,4 @@
+set -x
+git checkout upstream
+git pull --force --allow-unrelated-histories upstream master
+set +x
---
+++ setrefupstream.sh
@@ -0,0 +1,4 @@
+git remote add upstream https://github.com/just-containers/s6-overlay 2>/dev/null
+git remote set-url --push upstream fetchonly 2>/dev/null
+git remote add origin https://HowardMei@github.com/HowardMei/s6overlayscripts 2>/dev/null
+git remote -v
